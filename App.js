c63a333b8a5cc8a9a07af4c89e36b0f33ab268f9
pohjie <pohjie.pj@gmail.com>
2021-09-14 13:52:43 +0800

    working SearchBarScreen

--- App.js
+++ App.js
@@ -2,12 +2,20 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import SearchBarScreen from './screens/SearchBarScreen';
+
+const Stack = createNativeStackNavigator()
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="SearchBar">
+        <Stack.Screen name="SearchBar" component={SearchBarScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
